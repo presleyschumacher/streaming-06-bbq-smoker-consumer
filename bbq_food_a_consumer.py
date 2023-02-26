4b5df7f7210c0926938271efc970644caf1c97c6
presleyschumacher <presley.schumacher2@gmail.com>
2023-02-26 10:36:44 -0600

    initial commit

---
+++ bbq_food_a_consumer.py
@@ -0,0 +1,108 @@
+"""
+    This program listens for work messages contiously. 
+    
+    The bbq_producer.py must run to start sending the messge first
+    We want know if (Condition To monitor):
+        Food Temperature changes less than 1 degree in 10 minutes
+        
+### Name:  Presley Schumacher
+### Date:  February 21, 2023
+"""
+import pika
+import sys
+import time
+from collections import deque
+
+#Declare Deque
+# 1 reading every 30 seconds
+# 10 min * 1 / 0.5 min
+# The 20 most recent readings
+foodA_deque = deque(maxlen=20)
+
+# define a callback function to be called when a message is received
+def callback(ch, method, properties, body):
+    """ Define behavior on getting a message."""
+    # Message is decoded from bytes to a string using decode()
+    print(f" [x] Received {body.decode()}")
+    reading_string=body.decode()
+    # Split the reading_string variable using the split() method
+    # Retrieve the second element (index 1) assigned to the 'temp' variable
+    try:
+        temp=reading_string.split(",")[1]
+        smoker_deque.append(float(temp))
+        # If there are elements in smoker_deque the code checks if the difference
+        # between the max and min values in the deque is greater than or equal to 15.
+        # If the condition is met, the code prints a message indicating that the smoker temp has decreased by 15 degrees or more
+        if len(foodA_deque)==20 and max(foodA_deque)-min(foodA_deque)<1:
+            print("FOOD STALL ALERT! Food temp has changed by 1 degree or less in 10 min")
+
+    # Acknowledge that the message has been processed and can be removed from the queue    
+    except ValueError:
+        pass
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+
+# define a main function to run the program
+def main(hn: str = "localhost", qn: str = "task_queue"):
+    """ Continuously listen for task messages on a named queue."""
+
+    # when a statement can go wrong, use a try-except block
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=hn))
+
+    # If there's an error:
+    except Exception as e:
+        print()
+        print("ERROR: connection to RabbitMQ server failed.")
+        print(f"Verify the server is running on host={hn}.")
+        print(f"The error says: {e}")
+        print()
+        sys.exit(1)
+
+    try:
+        # use the connection to create a communication channel
+        channel = connection.channel()
+
+        # use the channel to declare a durable queue
+        # a durable queue will survive a RabbitMQ server restart
+        # and help ensure messages are processed in order
+        # messages will not be deleted until the consumer acknowledges
+        channel.queue_declare(queue=qn, durable=True)
+
+        # prefetch_count = Per consumer limit of unaknowledged messages      
+        channel.basic_qos(prefetch_count=1) 
+
+        # configure the channel to listen on a specific queue,  
+        # use the callback function named callback,
+        # and do not auto-acknowledge the message (let the callback handle it)
+        channel.basic_consume( queue=qn, on_message_callback=callback)
+
+        # print a message to the console for the user
+        print(" [*] Ready for work. To exit press CTRL+C")
+
+        # start consuming messages via the communication channel
+        channel.start_consuming()
+
+    # If there's an error:
+    except Exception as e:
+        print()
+        print("ERROR: something went wrong.")
+        print(f"The error says: {e}")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print()
+        print(" User interrupted continuous listening process.")
+        sys.exit(0)
+    finally:
+        print("\nClosing connection. Goodbye.\n")
+        connection.close()
+        
+
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__":
+    # call the main function with the information needed
+    main('localhost', 'food_a_queue')
+
